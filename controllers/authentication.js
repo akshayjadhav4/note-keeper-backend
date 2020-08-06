6f516de16daf15f25ea7446d28a282fc8e575d47
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-06 17:16:09 +0530

    protecting route

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -66,3 +66,19 @@ exports.signout = (req, res) => {
     message: "User signout successfully",
   });
 };
+
+//protecting route
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth",
+});
+
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+  next();
+};
