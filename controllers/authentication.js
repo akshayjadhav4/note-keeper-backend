59eb50a46b76c429006e694681a9a770697b6c7d
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-06 16:48:12 +0530

    sign in works

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
-
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 exports.signup = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -24,6 +25,37 @@ exports.signup = (req, res) => {
       email: user.email,
       id: user._id,
     });
+  });
+};
+
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+      param: errors.array()[0].param,
+    });
+  }
+  User.findOne({ email }, (error, user) => {
+    if (error || !user) {
+      return res.status(400).json({
+        error: "User account does not exists...",
+      });
+    }
+
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and Password do not match",
+      });
+    }
+    //CREATE TOKEN
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    // PUT TOKEN IN COOKIE
+    res.cookie("token", token, { expire: new Date() + 20 });
+    //SEND RESPONSE TO FRONTEND
+    const { _id, firstName, lastName, email } = user;
+    return res.json({ token, user: { _id, firstName, lastName, email } });
   });
 };
 
--- routes/authentication.js
+++ routes/authentication.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var router = express.Router();
 const { check } = require("express-validator");
-const { signup, signout } = require("../controllers/authentication");
+const { signup, signin, signout } = require("../controllers/authentication");
 
 router.post(
   "/signup",
@@ -13,6 +13,17 @@ router.post(
     check("email").isEmail().withMessage("provide valid email"),
   ],
   signup
+);
+
+router.post(
+  "/signin",
+  [
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("must be at least 5 chars long"),
+    check("email").isEmail().withMessage("provide valid email"),
+  ],
+  signin
 );
 
 router.get("/signout", signout);
