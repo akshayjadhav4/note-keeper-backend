ce4a816cfe51d8bae85b25b9d7578bf799fb1144
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-06 15:15:07 +0530

    middlewares added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const express = require("express");
 require("dotenv").config();
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 
 //db connection
@@ -14,6 +16,11 @@ mongoose
   .then(() => {
     console.log("DB CONNECTED");
   });
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 const port = process.env.PORT || 2004;
 
